802cb53bad43d87eee582070472134baa738f5c0
Brandon Vera <108695519+BrandonVeraWeb@users.noreply.github.com>
2023-06-13 10:21:19 -0400

    refactor: add error validation (#1)

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function genQR() {
-  const gapi = "https://chart.googleapis.com/chart?cht=qr&chs=";
+  const googApi = "https://chart.googleapis.com/chart?cht=qr&chs=";
   const myImg = document.getElementById("img");
   const myText = document.getElementById("qrtext").value;
   const mySize = document.getElementById("size").value;
@@ -10,27 +10,30 @@ function genQR() {
 
   switch (mySize) {
     case "100":
-      myImg.src = gapi + "100x100" + "&chl=" + myText;
+      myImg.src = googApi + "100x100" + "&chl=" + myText;
       break;
     case "150":
-      myImg.src = gapi + "150x150" + "&chl=" + myText;
+      myImg.src = googApi + "150x150" + "&chl=" + myText;
       break;
     case "200":
-      myImg.src = gapi + "200x200" + "&chl=" + myText;
+      myImg.src = googApi + "200x200" + "&chl=" + myText;
       break;
     case "250":
-      myImg.src = gapi + "250x250" + "&chl=" + myText;
+      myImg.src = googApi + "250x250" + "&chl=" + myText;
       break;
     case "300":
-      myImg.src = gapi + "300x300" + "&chl=" + myText;
+      myImg.src = googApi + "300x300" + "&chl=" + myText;
       break;
     default:
       throw new Error("The size can not be empty");
-      
   }
 }
 const generateQr = document.getElementById("generate-qr");
 
 generateQr.addEventListener("click", () => {
-  genQR();
+  try {
+    genQR();
+  } catch (error) {
+    alert(error.message);
+  }
 });
